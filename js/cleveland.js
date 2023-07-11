ba9474da95213f01d2ac49cf54fc5ecf0e699af9
Jayllen <jhathman@metrohealth.org>
2023-07-11 11:20:36 -0400

    js file

---
+++ js/cleveland.js
@@ -0,0 +1,20 @@
+/*window.onload = init;
+
+var dollarPng = document.getElementById("dollarIMG").width = "500px";
+
+
+document.querySelector(".OH").onclick = OHclick;
+document.querySelector(".NY").onclick = NYclick;
+document.querySelector(".TX").onclick = TXclick;
+document.querySelector(".FL").onclick = FLclick;
+document.querySelector(".PA").onclick = PAclick;
+
+
+let OHclick =()=>{ dollarPng.width = dollarPng.width*1.13;}
+let NYclick =()=>{ dollarPng.width = dollarPng.width*.86;}
+let TXclick =()=>{ dollarPng.width = dollarPng.width*1.03;}
+let FLclick =()=>{ dollarPng.width = dollarPng.width*.99;}
+let PAclick =()=>{ dollarPng.width = dollarPng.width*1.03;}
+
+init();
+*/
