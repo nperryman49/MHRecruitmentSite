1d9e66fa37f547254da659306de7a006c27de48f
Hershey S Greenspan <hersheyg1992@gmail.com>
2023-08-02 16:22:29 -0400

    php changes.

--- php/form_script.php
+++ php/form_script.php
@@ -1,12 +1,30 @@
 <?php
 
 $email=$_Post['email'];
-$name=$_Post['name'];
-$data=$name.",".email;
+$text=$_Post['lname'];
+$text=$_Post['fname'];
+$tel=$_Post['phone'];
+$text=$_Post['country_code'];
+$file=$_Post['files'];
+$nurse_status=$_Post['Nurse_Status'];
+$date=$_Post['datemin'];
+$time=$_Post['appt'];
+$date=$_Post['datemin'];
+$time=$_Post['appt'];
+$appt=$_Post['appt'];
+$data=$email."," $text."," $text."," $tel."," $text."," $file."," $nurse_status."," $date."," $time."," $date."," $time."," $appt;
 
-$file="file.csv";
+$file="nurseinfo.xlsx";
 
 file_put_contents($file, $data . PHP_EOL, FILE_APPEND);
-print "<h1 align=center>Thank you for submitting your email address!</h1>";
+print "<h1 align=center>Thank you for submitting your contact information! a recruiter will contact you shortly.</h1>";
 
 ?>
+<html>
+<head>
+    <title>Thank you page</title>
+</head>
+<body>
+<h1><a href="contactus.html">Go back.</a></h1>
+</body>
+</html>
